b8f6ce52ce392cd7f3b1b9307e95a8b87b96e831
DavidPToyo <davidpichardot31@hotmail.com>
2025-05-10 02:11:16 +0000

    integrar tooltip con bootstrap js en boton enviar

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const myCarouselElement = document.querySelector('carouselExample')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 400,
+  touch: false
+})
